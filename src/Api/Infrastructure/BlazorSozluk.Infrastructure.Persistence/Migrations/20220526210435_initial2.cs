8d5701af19a3f6ada6fa6e4daa666d15e5be20a3
berkctezc <berkcantezcaner@gmail.com>
2022-05-27 00:19:17 +0300

    updated migration

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Migrations/20220526210435_initial2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorSozluk.Infrastructure.Persistence.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Users");
+        }
+    }
+}
